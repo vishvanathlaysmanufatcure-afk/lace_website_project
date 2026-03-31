910b50bd47d6606fbd16a04437740ef116e1c876
vishvanathlaysmanufatcure-afk <vishvanathlaysmanufatcure@gmail.com>
2026-04-01 01:36:08 +0530

    Fix: admin page useSearchParams build error

--- src/app/admin-secret-933/add-product/page.js
+++ src/app/admin-secret-933/add-product/page.js
@@ -3,10 +3,13 @@
 import { useState } from "react";
 import { uploadImage } from "@/lib/cloudinary";
 import { addProduct } from "@/services/productService";
-import { useRouter} from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export default function AddProduct() {
   const router = useRouter();
+
+  const searchParams = useSearchParams();
+  const previousCategory = searchParams.get("category");
 
   const [category, setCategory] = useState("");
   const [categoryLabel, setCategoryLabel] = useState("Select the category");
@@ -75,7 +78,11 @@ export default function AddProduct() {
       });
 
       alert("Product Added");
-      router.push("/admin-secret-933");
+      router.push(
+        previousCategory
+          ? `/admin-secret-933?category=${previousCategory}`
+          : `/admin-secret-933`,
+      );
     } catch (error) {
       console.error(error);
       alert("Error uploading product");
--- src/app/admin-secret-933/page.js
+++ src/app/admin-secret-933/page.js
@@ -1,3 +1,5 @@
+//src/app/admin-secret-933/page.js
+export const dynamic = "force-dynamic";
 "use client";
 
 import { useEffect, useState } from "react";
