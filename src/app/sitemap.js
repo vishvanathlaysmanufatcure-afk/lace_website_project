5b7935f52433a625122974ecb79d25cd182b4601
vishvanathlaysmanufatcure-afk <vishvanathlaysmanufatcure@gmail.com>
2026-04-01 20:42:12 +0530

    Add sitemap

---
+++ src/app/sitemap.js
@@ -0,0 +1,24 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://www.lacemanufactures.in",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.lacemanufactures.in/about",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.lacemanufactures.in/products",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.lacemanufactures.in/services",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://www.lacemanufactures.in/contact",
+      lastModified: new Date(),
+    },
+  ];
+}
